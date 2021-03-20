3876ac1d4461cb78071880f8a33a389e94bf0728
Anshika15 <bansalanshika20@gmail.com>
2021-03-20 20:57:51 +0530

    Application Context

--- src/main/java/com/practice/SpringDemo/App.java
+++ src/main/java/com/practice/SpringDemo/App.java
@@ -2,13 +2,16 @@ package com.practice.SpringDemo;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class App 
 {
     public static void main( String[] args )
     {
-    	BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+//    	BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
         Alien obj = (Alien)factory.getBean("alien");
         obj.code();
     }
